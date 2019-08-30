c2ffa9c756afbb7b046d14ab49a19602f28213f6
김기성 <kkimkisung@gmail.com>
2019-08-30 20:06:04 +0900

    last for byungki

--- Shinple/Table/LEC_CATE.swift
+++ Shinple/Table/LEC_CATE.swift
@@ -9,17 +9,17 @@ import AWSAuthCore
 class LEC_CATE: AWSDynamoDBObjectModel, AWSDynamoDBModeling {
     
     var _LECTURE: String?
-    var _개발 : Any?
-    var _금융 : Any?
-    var _문화 : Any?
-    var _어학 : Any?
-    var _육아 : Any?
-    var _자격증 : Any?
-    var _필수 : Any?
+    var _develop : Any?
+    var _Finance : Any?
+    var _Culture : Any?
+    var _English : Any?
+    var _Care : Any?
+    var _Certificate : Any?
+    var _Duty : Any?
     
     
     class func dynamoDBTableName() -> String {
-        return "EMPLOYEE"
+        return "LEC_CATE"
     }
     
     class func hashKeyAttribute() -> String {
@@ -29,13 +29,13 @@ class LEC_CATE: AWSDynamoDBObjectModel, AWSDynamoDBModeling {
     override class func jsonKeyPathsByPropertyKey() -> [AnyHashable: Any] {
         return [
             "_LECTURE" : "LECTURE",
-            "_개발" : "개발",
-            "_금융" : "금융",
-            "_문화" : "문화",
-            "_어학" : "어학",
-            "_육아": "육아",
-            "_자격증" : "자격증",
-            "_필수" : "필수"
+            "_develop" : "develop",
+            "_Finance" : "Finance",
+            "_Culture" : "Culture",
+            "_English" : "English",
+            "_Care": "Care",
+            "_Certificate" : "Certificate",
+            "_Duty" : "Duty"
         ]
     }
     
@@ -70,9 +70,8 @@ func queryLEC_CATE() {
         if output != nil {
             for cate in output!.items {
                 let cateItem = cate as? LEC_CATE
-                print(type(of:cateItem!._금융))
-                var dict:NSDictionary = cateItem!._금융 as! NSDictionary
-                print(dict.allKeys)
+                print(type(of:cateItem!._develop))
+                print(cateItem)
                 
             }
         }
